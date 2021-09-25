500ea89a9e39678c956a4ee28bfd57f3f4fa8d54
Dmitriy Nozhkin <dmitrilay@gmail.com>
2021-09-25 16:12:27 +0600

    Перенес интерфейс командной строки в отдельный файл

--- ParsFile.py
+++ ParsFile.py
@@ -19,13 +19,12 @@ class ParserFile:
         self.db = db
         self.path_to_open = path_to_open
         self.path_to_save = path_to_save
-        self.price_db_new = {'Наименование': [], 'Цена': []}
-        self.price_db = {'Наименование': [], 'Цена': [], 'Цена2': [], 'дата': [], 'Вывод': []}
-        self.price_db_exit = {'Наименование': [], 'Цена': []}
-        self.price_dict_exit = {'Наименование': [], 'Цена': []}
+        # self.price_db_new = {'Наименование': [], 'Цена': []}
+        self.price_db = {}
+        # self.price_db_exit = {'Наименование': [], 'Цена': []}
+        # self.price_dict_exit = {'Наименование': [], 'Цена': []}
         self.dict_price_entrance = {}
-
-        self.price_db_update = {'Наименование': [], 'Цена': []}
+        # self.price_db_update = {'Наименование': [], 'Цена': []}
 
     def open_file(self, file=None):
         """
@@ -86,11 +85,8 @@ class ParserFile:
         print('-- Выгружаю данные из базы')
         with self.db.db:
             for data in self.db.PriceParser.select():
-                self.price_db['Наименование'].append(data.name)
-                self.price_db['Цена'].append(data.price_old)
-                self.price_db['Цена2'].append(data.price_new)
-                self.price_db['дата'].append(data.date_recording)
-                self.price_db['Вывод'].append(data.display)
+                d = data
+                self.price_db[d.name] = [d.price_old, d.price_new, d.date_recording, d.display]
 
     def find_in_the_database(self):
         """
@@ -201,67 +197,3 @@ class ParserFile:
             ws.cell(row=i + 1, column=5, value=self.price_db['Вывод'][i])
 
         wb.save(filename=path_to_save)
-
-
-if __name__ == "__main__":
-    list_db = [[db1, 'mvideo.xlsx'], [db2, 'mts.xlsx'], [db3, 'dns.xlsx'], [db4, 'eldorado.xlsx']]
-    action3 = input('Выберите базу данных:\n'
-                    '1)МВИДЕО\n'
-                    '2)МТС\n'
-                    '3)ДНС\n'
-                    '4)ЭЛЬДОРАДО\n')
-
-    if action3 == '1':
-        v_db = list_db[0][0]
-        mg = list_db[0][1]
-        t2 = (os.path.join(PathOpen, list_db[0][1]))
-    elif action3 == '2':
-        v_db = list_db[1][0]
-        mg = list_db[1][1]
-        t2 = (os.path.join(PathOpen, list_db[1][1]))
-    elif action3 == '3':
-        v_db = list_db[2][0]
-        mg = list_db[2][1]
-        t2 = (os.path.join(PathOpen, list_db[2][1]))
-    elif action3 == '4':
-        v_db = list_db[3][0]
-        mg = list_db[3][1]
-        t2 = (os.path.join(PathOpen, list_db[3][1]))
-
-    pm = ParserFile(path_to_open=t2, path_to_save=PathSave, db=v_db, mg=mg)
-    action = input('Выбери действие:\n '
-                   '1) Обновить db из файл(данные парсинга)\n '
-                   '2) Импорт базы данных на сервер\n '
-                   '3) Выгрузить базу и сохранить в excel\n '
-                   '4) Exit\n ')
-
-    if action == '1':
-        text, i2 = '', 0
-        for i in os.listdir(PathOpen):
-            text = text + f'{i2 + 1})' + i + '\n'
-            i2 = i2 + 1
-        action2 = input(f'Пожалуйста выбери файл из списка:\n{text}')
-        t1 = os.listdir(PathOpen)[int(action2) - 1]
-        path_data = (os.path.join(PathOpen, t1))
-
-        pm.open_file(path_data)
-        pm.unloading_from_the_database()
-        pm.find_in_the_database()
-    elif action == '2':
-        path_data = (os.path.join(os.getcwd(), 'data'))
-        text = ''
-        i2 = 0
-        for i in os.listdir(path_data):
-            text = text + f'{i2 + 1})' + i + '\n'
-            i2 = i2 + 1
-        action2 = input(f'\nПожалуйста выбери файл из списка:\n\n{text}')
-        t1 = os.listdir(path_data)[int(action2) - 1]
-        path_data = (os.path.join(path_data, t1))
-        pm.open_file(path_data)
-        pm.unloading_from_the_database()
-        pm.find_in_the_database()
-    elif action == '3':
-        pm.unloading_from_the_database()
-        pm.writing_file_excel_db()
-    elif action == '4':
-        pass
